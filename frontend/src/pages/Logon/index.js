c0accc902ed8af00e53a9d22fa33c176e7a7c555
Johnny Silva <johnnysilva.dev@gmail.com>
2020-03-27 16:29:37 -0300

    tela-register

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
@@ -16,10 +17,10 @@ export default function Logon() {
 
                     <input placeholder="Sua ID"/>
                     <button className="button" type="submit">Entrar</button>
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041" />
                         Não tenho cadastro
-                        </a>
+                    </Link>
                 </form>
             </section>
             <img src={heroesImg} alt="Heroes"/>
